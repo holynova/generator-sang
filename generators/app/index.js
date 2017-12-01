672b7752ebd411324333c907044de01fb7ed2347
ymsang <ymsang@mo9.com>
2017-12-01 17:59:05 +0800

    重新整理出2个模板,4种类型, 版本号0.2.0

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,25 +1,36 @@
 'use strict';
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
-const yosay = require('yosay');
+const path = require('path')
+// Const yosay = require('yosay');
+const typeDict = {
+  1: { title: '移动端Html5 + webpack', tplPath: 'pc_webpack' },
+  2: { title: 'PC端网页 + webpack', tplPath: 'pc_webpack' },
+  3: { title: '移动端Html5 + gulp', tplPath: 'pc_gulp' },
+  4: { title: 'PC端网页 + gulp', tplPath: 'pc_gulp' },
+}
 
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
-    this.log(
-      yosay(
-        '请先建立文件夹, cd到项目文件夹, 再运行 yo sang ' + chalk.red('generator-sang')
-      )
-    );
+    this.log('请先建立文件夹, cd到项目文件夹, 再运行 yo sang');
+    let message = "选择项目类型:\n"
+    let keys = Object.keys(typeDict)
+    for (let i = 0; i < keys.length; i++) {
+      let key = keys[i]
+      let str = `${key}: ${typeDict[key].title} \n`
+      message += str
+    }
 
     const prompts = [
       {
         type: 'input',
-        name: 'projectName',
-        message: '输入项目名(默认为文件夹名)',
-        Default: this.appname
+        name: 'projectType',
+        message,
+        Default: 1
       }
     ];
+
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
       this.props = props;
@@ -27,23 +38,32 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    this.fs.copy(this.templatePath('all'), this.destinationPath());
-    // This.fs.copy(this.templatePath('all/src'), this.destinationPath('src'));
-    this.fs.copy(this.templatePath('tools/_.babelrc'),
-      this.destinationPath('.babelrc'));
-    this.fs.copy(
-      this.templatePath('tools/_.eslintrc.js'),
-      this.destinationPath('.eslintrc.js')
-    );
-    this.fs.copy(
-      this.templatePath('tools/_.gitignore'),
-      this.destinationPath('.gitignore')
-    );
-    // This.fs.copy(this.templatePath('all/.babelrc'), this.destinationPath('all/.babelrc'));
+    const type = this.props.projectType
+    let settings = typeDict[type]
+    if (settings) {
+      let { tplPath } = settings
+      this.fs.copy(this.templatePath(tplPath), this.destinationPath());
+    }
+  }
+  //复制以点开头的特殊文件
+  copyTools() {
+    const toolsPath = 'tools'
+    const files = [
+      { srcName: '_.babelrc', destName: '.babelrc' },
+      { srcName: '_.eslintrc', destName: '.eslintrc' },
+      { srcName: '_.gitignore', destName: '.gitignore' },
+    ]
+    for (let i = 0; i < files.length; i++) {
+      let { srcName, destName } = files[i]
+      // let fromPath = path.resolve(toolsPath, srcName)
+      let fromPath = `${toolsPath}/${srcName}`
+      this.fs.copy(
+        this.templatePath(fromPath),
+        this.destinationPath(destName));
+    }
   }
 
   install() {
-    // this.installDependencies();
     this.npmInstall();
   }
 };
