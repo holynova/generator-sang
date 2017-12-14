c0013569725c18c0eb516d74338645c9feca50cf
ymsang <ymsang@mo9.com>
2017-12-14 16:09:56 +0800

    修改gulp类型中的引用css文件错误, 增加移动端gulp模板

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,25 +1,25 @@
 'use strict';
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
-const path = require('path')
+const path = require('path');
 // Const yosay = require('yosay');
 const typeDict = {
   1: { title: '移动端Html5 + webpack', tplPath: 'mobile_webpack' },
   2: { title: 'PC端网页 + webpack', tplPath: 'pc_webpack' },
-  3: { title: '移动端Html5 + gulp', tplPath: 'pc_gulp' },
-  4: { title: 'PC端网页 + gulp', tplPath: 'pc_gulp' },
-}
+  3: { title: '移动端Html5 + gulp', tplPath: 'mobile_gulp' },
+  4: { title: 'PC端网页 + gulp', tplPath: 'pc_gulp' }
+};
 
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
     this.log('请先建立项目文件夹, cd到项目文件夹, 再运行命令: yo sang');
-    let message = "选择项目类型:\n"
-    let keys = Object.keys(typeDict)
+    let message = '选择项目类型:\n';
+    let keys = Object.keys(typeDict);
     for (let i = 0; i < keys.length; i++) {
-      let key = keys[i]
-      let str = `${key}: ${typeDict[key].title} \n`
-      message += str
+      let key = keys[i];
+      let str = `${key}: ${typeDict[key].title} \n`;
+      message += str;
     }
 
     const prompts = [
@@ -38,27 +38,27 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    const type = this.props.projectType
-    // this.log('this.props.projectType' + this.props.projectType)
+    const type = this.props.projectType;
+    // This.log('this.props.projectType' + this.props.projectType)
 
-    let settings = typeDict[type]
-    // this.log('settings', settings)
+    let settings = typeDict[type];
+    // This.log('settings', settings)
     if (settings) {
       this.fs.copy(this.templatePath(settings.tplPath), this.destinationPath());
     }
   }
-  //复制以点开头的特殊文件
+  // 复制以点开头的特殊文件
   copyTools() {
-    const toolsPath = 'tools'
+    const toolsPath = 'tools';
     const files = [
       { srcName: '_.babelrc', destName: '.babelrc' },
       { srcName: '_.eslintrc.js', destName: '.eslintrc.js' },
-      { srcName: '_.gitignore', destName: '.gitignore' },
-    ]
+      { srcName: '_.gitignore', destName: '.gitignore' }
+    ];
     for (let i = 0; i < files.length; i++) {
-      let { srcName, destName } = files[i]
-      // let fromPath = path.resolve(toolsPath, srcName)
-      let fromPath = `${toolsPath}\\${srcName}`
+      let { srcName, destName } = files[i];
+      // Let fromPath = path.resolve(toolsPath, srcName)
+      let fromPath = `${toolsPath}\\${srcName}`;
       this.fs.copy(
         this.templatePath(fromPath),
         this.destinationPath(destName));
@@ -66,7 +66,7 @@ module.exports = class extends Generator {
   }
 
   install() {
-    this.log('正在安装依赖包, 你可以选择手动中止, 手动用cnpm来安装以加快速度')
+    this.log('正在安装依赖包, 你可以选择手动中止, 手动用cnpm来安装以加快速度');
     this.npmInstall();
   }
 };
---
+++ generators/app/templates/mobile_gulp/gulpfile.js
@@ -0,0 +1,100 @@
+const del = require('del');
+const gulp = require('gulp');
+const $ = require('gulp-load-plugins')();
+
+const srcPath = './src/';
+const destPath = './dist/';
+const revPath = './rev/';
+// const version = '1.1.0';
+
+const log = console.log;
+
+
+const filter = {
+  less: `${srcPath}/styles/*.less`,
+  scss: `${srcPath}/styles/*.scss`,
+  css: `${srcPath}/styles/*.css`,
+  html: `${srcPath}*.html`,
+  img: `${srcPath}images/*.*`,
+  js: `${srcPath}/scripts/*.js`,
+};
+
+
+gulp.task('del', (cb) => {
+  del(destPath).then(() => {
+    log('dest dir clear');
+    cb();
+  });
+});
+gulp.task('js', (cb) => {
+  gulp.src(filter.js)
+    .pipe($.debug())
+    .pipe($.babel({ presets: ['env'] }))
+    .pipe($.uglify())
+    .pipe($.rev())
+    .pipe(gulp.dest(`${destPath}/js`))
+    .pipe($.rev.manifest())
+    .pipe(gulp.dest(`${revPath}/js`))
+    .on('end', cb);
+});
+
+gulp.task('less', (cb) => {
+  gulp.src(filter.less)
+    .pipe($.less())
+    .pipe($.csso())
+    .pipe($.rev())
+    .pipe(gulp.dest(`${destPath}/css`))
+    .pipe($.rev.manifest())
+    .pipe(gulp.dest(`${revPath}/less`))
+    .on('end', cb);
+});
+gulp.task('scss', (cb) => {
+  gulp.src(filter.scss)
+    .pipe($.sass())
+    .pipe($.csso())
+    .pipe($.rev())
+    .pipe(gulp.dest(`${destPath}/css`))
+    .pipe($.rev.manifest())
+    .pipe(gulp.dest(`${revPath}/scss`))
+    .on('end', cb);
+});
+gulp.task('css', (cb) => {
+  gulp.src(filter.css)
+    .pipe($.csso())
+    .pipe($.rev())
+    .pipe(gulp.dest(`${destPath}/css`))
+    .pipe($.rev.manifest())
+    .pipe(gulp.dest(`${revPath}/css`))
+    .on('end', cb);
+});
+
+gulp.task('image', (cb) => {
+  gulp.src(filter.img)
+    .pipe($.imagemin())
+    .pipe($.rev())
+    .pipe(gulp.dest(`${destPath}/imgs`))
+    .pipe($.rev.manifest())
+    .pipe(gulp.dest(`${revPath}/imgs`))
+    .on('end', cb);
+});
+
+gulp.task('html', (cb) => {
+  gulp.src([`${revPath}/**/*.json`, filter.html])
+    .pipe($.revCollector())
+    .pipe($.htmlmin({ collapseWhitespace: true, removeComments: true }))
+    .pipe(gulp.dest(destPath))
+    .on('end', cb);
+});
+gulp.task('delRev', (cb) => {
+  del(revPath).then(() => {
+    cb();
+  });
+});
+
+gulp.task('watch', () => {
+  gulp.watch([filter.less, filter.html, filter.img], ['debug']);
+  log('watching...');
+  // content
+});
+
+gulp.task('default', $.sequence('del', 'less', 'scss', 'css', 'js', 'html', 'delRev'));
